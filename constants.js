d6b955d7a8adeba06c9e8287c558d9bcf55323f4
Roman B <onwork.r@yandex.ru>
2022-12-09 21:30:12 +0300

    addConstants

---
+++ constants.js
@@ -0,0 +1,20 @@
+const BAD_REQUSET = 400;
+const NOT_FOUND = 404;
+const SERVER_ERROR = 500;
+const OK = 200;
+const CREATED = 201;
+
+const ERROR_400 = 'Переданы некорректные данные';
+const ERROR_404 = 'Запрашиваемый пользователь не найден';
+const ERROR_500 = 'На сервере произошла ошибка';
+
+module.exports = {
+  BAD_REQUSET,
+  NOT_FOUND,
+  SERVER_ERROR,
+  OK,
+  CREATED,
+  ERROR_400,
+  ERROR_404,
+  ERROR_500,
+};
--- controllers/cards.js
+++ controllers/cards.js
@@ -1,14 +1,15 @@
 const Card = require('../modeles/card'); // импорт моделе с соответствующей схемой
 
-const BAD_REQUSET = 400;
-const NOT_FOUND = 404;
-const SERVER_ERROR = 500;
-const OK = 200;
-const CREATED = 201;
-
-const ERROR_400 = 'Переданы некорректные данные';
-const ERROR_404 = 'Запрашиваемый пользователь не найден';
-const ERROR_500 = 'Произошла ошибка';
+const {
+  BAD_REQUSET,
+  NOT_FOUND,
+  SERVER_ERROR,
+  OK,
+  CREATED,
+  ERROR_400,
+  ERROR_404,
+  ERROR_500,
+} = require('../constants');
 
 // сработает при GET-запросе на URL /cards - возвращает всех пользователей
 module.exports.getCards = (req, res) => {
